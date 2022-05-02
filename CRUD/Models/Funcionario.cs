0c58164a92e493cc0f04867d7f80e9771e238db3
Xandre <xandredepaula2312@gmail.com>
2022-05-02 18:30:28 -0300

    Configurando ambiente e fazendo a conexão com o banco

---
+++ CRUD/Models/Funcionario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.Models
+{
+    public class Funcionario
+    {
+        
+        public int ID { get; set; } 
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public int Matricula { get; set; }
+        [Required]
+        public int Posicao { get; set; }
+    }
+}
